87908ee1f0d7ac2d5695ef6f75ead5eae19adad2
Vanyaosta <ivanostaschuk@gmail.com>
2025-08-06 17:02:18 +0300

    зводимо у ступінь

--- js_homework.js
+++ js_homework.js
@@ -1,2 +1,13 @@
-pow(x,y)
-pow(2,3);
+// pow(x,y)
+// pow(2,3);
+
+function pow(x, y) {
+let result = 1;
+for (let i = 0; i < y; i++) {
+    result = result * x;
+    }
+    return result;
+}
+console.log(pow(2,3));
+console.log(pow(5,0));
+console.log(pow(3,5));
