fcc6c5ce5b28845118297ced756a8ae564169dfc
Vanyaosta <ivanostaschuk@gmail.com>
2025-08-26 14:27:29 +0300

    without A

--- js_homework.js
+++ js_homework.js
@@ -1,52 +1,9 @@
 "use strict"
 
-var services = {
-    "стрижка": "60 грн",
-    "гоління": "80 грн",
-    "Миття голови": "100 грн"
-  };
-  
-  services['Розбити скло'] = "200 грн";
-  services['Ремонт скла'] = "250 грн";
-  services['Поставити своє скло'] = "300 грн";
-  
-  services.price = function() {  // загальна ціна
-    let sum = 0;
-    for (let serviceName in this) { 
-      if (typeof this[serviceName] === "string") { 
-        sum = sum + parseFloat(services[serviceName]); 
-      }
-    }
-    return sum + " грн";
-  };
-  
-  services.minPrice = function() {    // мінімальна ціна
-    let min = Infinity;
-    for (let serviceName in this) {
-      if (typeof this[serviceName] === "string") {
-        let value = parseFloat(this[serviceName]);
-        if (value < min) {
-          min = value;
-        }
-      }
-    }
-    return min + " грн";
-  };
-  
-  services.maxPrice = function() { // максимальна ціна
-    let max = -Infinity;
-    for (let serviceName in this) {
-      if (typeof this[serviceName] === "string") {
-        let value = parseFloat(this[serviceName]);
-        if (value > max) {
-          max = value;
-        }
-      }
-    }
-    return max + " грн";
-  };
-  
-  console.log("Загальна ціна:", services.price());
-  console.log("Мінімальна ціна:", services.minPrice());
-  console.log("Максимальна ціна:", services.maxPrice());
-  
+var words = "Wonderful Happiness Joyful Time Task Apple";
+
+var re = /\b[^aA]{6,}\b/g;
+
+var withoutA = words.match(re);
+
+console.log(withoutA); 
